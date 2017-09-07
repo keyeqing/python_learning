164d728e462d13867e833f2b466f1f7ae0282d7c
Yeqing Ke <yeke@microsoft.com>
2017-09-07 09:40:10 +0800

    learning numpy & pandas

--- numpy_pandas/test_numpy.py
+++ numpy_pandas/test_numpy.py
@@ -11,6 +11,8 @@ f = np.arange(10,20,2)
 g = np.arange(12).reshape((3,4))
 h = np.linspace(1,10,20)
 
+
+
 print(a.dtype)
 print(b.shape)
 print(c)
@@ -19,3 +21,53 @@ print(e)
 print(f)
 print(g)
 print(h)
+
+A = np.arange(14,2,-1).reshape(3,4)
+
+print(A)
+print(np.clip(A,5,9))#截断
+
+##index
+
+print(A[2])
+print(A[1,1:3])
+print(A.flatten())
+
+##数组合并
+A = np.array([1,1,1])
+B = np.array([2,2,2])
+
+print(np.vstack((A,B)))
+print(np.hstack((A,B)))
+print(A)
+print(A[np.newaxis,:])
+print(A[:,np.newaxis])
+A = np.array([1,1,1])[:,np.newaxis]
+B = np.array([2,2,2])[:,np.newaxis]
+C = np.concatenate((A,B,B,A), axis=1)
+print("con:")
+print(A)
+print(C)
+
+##数组split
+
+A= np.arange(12).reshape((3,4))
+
+print(np.split(A,3,axis=0))
+print(np.array_split(A,3,axis=1))
+
+##copy
+
+a = np.arange(4)
+
+b=a
+a[0]=11
+b[1]=10
+print(a)
+print(b)
+
+c=a.copy()
+a[3]=11
+print(a)
+print(c)
+
---
+++ numpy_pandas/test_pandas.py
@@ -0,0 +1,87 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+#Series
+s = pd.Series([1,3,6,np.nan,44,1])
+
+print(s)
+print(s.dtype)
+
+#DataFrame
+
+dates = pd.date_range('20160101',periods=6)
+df = pd.DataFrame(np.random.rand(6,4),index=dates,columns=['a','b','c','d'])
+print(df)
+print(df['b'])
+
+df1=pd.DataFrame(np.arange(12).reshape(3,4))
+print(df1)
+print(df1.sort_index(axis=1,ascending=False))
+print(df1.sort_values(by=1, ascending=False))
+#导出
+
+data = pd.read_csv('students.csv')
+
+print(data)
+
+data.to_csv('s.csv')
+
+#join
+#定义资料集
+df1 = pd.DataFrame(np.ones((3,4))*0, columns=['a','b','c','d'], index=[1,2,3])
+df2 = pd.DataFrame(np.ones((3,4))*1, columns=['b','c','d','e'], index=[2,3,4])
+
+#依照`df1.index`进行横向合并
+res = pd.concat([df1, df2], axis=1, join_axes=[df1.index])
+print(df1)
+print(df2)
+print(res)
+
+#定义资料集并打印出
+left = pd.DataFrame({'key1': ['K0', 'K0', 'K1', 'K2'],
+                      'key2': ['K0', 'K1', 'K0', 'K1'],
+                      'A': ['A0', 'A1', 'A2', 'A3'],
+                      'B': ['B0', 'B1', 'B2', 'B3']})
+right = pd.DataFrame({'key1': ['K0', 'K1', 'K1', 'K2'],
+                       'key2': ['K0', 'K0', 'K0', 'K0'],
+                       'C': ['C0', 'C1', 'C2', 'C3'],
+                       'D': ['D0', 'D1', 'D2', 'D3']})
+
+print(left)
+#    A   B key1 key2
+# 0  A0  B0   K0   K0
+# 1  A1  B1   K0   K1
+# 2  A2  B2   K1   K0
+# 3  A3  B3   K2   K1
+
+print(right)
+#    C   D key1 key2
+# 0  C0  D0   K0   K0
+# 1  C1  D1   K1   K0
+# 2  C2  D2   K1   K0
+# 3  C3  D3   K2   K0
+
+#依据key1与key2 columns进行合并，并打印出四种结果['left', 'right', 'outer', 'inner']
+res = pd.merge(left, right, on=['key1', 'key2'], how='inner')
+print(res)
+#    A   B key1 key2   C   D
+# 0  A0  B0   K0   K0  C0  D0
+# 1  A2  B2   K1   K0  C1  D1
+# 2  A2  B2   K1   K0  C2  D2
+#画图
+# 随机生成1000个数据
+data = pd.DataFrame(
+    np.random.randn(1000,4),
+    index=np.arange(1000),
+    columns=list("ABCD")
+    )
+
+print(data)
+
+# 为了方便观看效果, 我们累加这个数据
+data.cumsum()
+
+# pandas 数据可以直接观看其可视化形式
+data.plot()
+
+plt.show()
